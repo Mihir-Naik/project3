12988f6e64517c83211daca9dbaddaa42efac4ef
Chase Morris <camorris@callutheran.edu>
2017-10-16 10:54:48 -0700

    updated Properties Controller

---
+++ controllers/property.js
@@ -0,0 +1,46 @@
+const
+// this variable allows us to retriever the data out of the models folder and then out of the file
+    Property = require('../models/Property.js')
+//this is where we set our controller functions for the routes. this helps us consolidate our files
+  module.exports = {
+    index: (req,res)=>{
+      Property.find({}, (err, property)=>{
+        if(err) return console.log(err)
+        
+        res.render('properties/index',{properties})
+      })
+    },
+
+    create: (req,res)=>{
+      var newProperty = new Property(req.body)
+      newProperty.save((err,property)=>{
+          if(err) return console.log(err)
+          res.redirect('/properties')
+      })
+    },
+
+    show:(req,res)=>{
+        Property.findById(req.params.id, (err, specificProperty)=>{
+           
+            res.render('properties/show', {property: specificProperty})
+        })
+    },
+
+    new:(req,res)=>{
+        res.render('properties/new')
+    },
+
+    destroy: (req,res)=>{
+        Property.findByIdAndRemove(req.params.id,(err, vacantProperty)=>{
+            if(err) return console.log(err)
+            res.json(vacantProperty)
+            res.json({success: true, message: "Vacant Property"})
+        })
+    },
+
+    edit: (req,res)=>{
+        Property.findByIdAndUpdate(req.params.id,(err, updatedProperty)=>{
+            res.json(updatedProperty)
+        })
+    }
+  }
