22ec7ef634008c46518ed85eb4318c5997f128a1
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-10-16 10:48:25 -0700

    Set up user routes and user controller actions

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 // Main server file for the project //
 const
-    express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser'),
-    ejs = require('ejs'),
-    ejsLayouts = require('express-ejs-layouts'),
-    flash = require('connect-flash'),
-    cookieParser = require('cookie-parser'),
-    session = require('express-session'),
-    MongoDBStore = require('connect-mongodb-session')(session),
-    passport = require('passport')
+  express = require('express'),
+  app = express(),
+  mongoose = require('mongoose'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  ejs = require('ejs'),
+  ejsLayouts = require('express-ejs-layouts'),
+  flash = require('connect-flash'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  MongoDBStore = require('connect-mongodb-session')(session),
+  passport = require('passport'),
+  userRoutes = require('./routes/users.js')
 
 // Environment PORT ///////// replace Project3 with project name once decided ⬇//////
 const
-    port = process.env.PORT || 3000,
-    mongoConnectionString = process.env.MONGODB_URL || 'mongodb://localhost/project3'
+  port = process.env.PORT || 3000,
+  mongoConnectionString = process.env.MONGODB_URL || 'mongodb://localhost/project3'
 
 // Establish Mongoose Connection
 mongoose.connect(mongoConnectionString, (err) => {
@@ -29,10 +30,10 @@ app.use(ejsLayouts)
 
 // Root Route
 app.get('/', (req,res) => {
-    res.render('index')
+  res.render('index')
 })
 
 // Server startup
 app.listen(port, (err) => {
-    console.log(err || `Server running on port: ${port}`)
+  console.log(err || `Server running on port: ${port}`)
 })
