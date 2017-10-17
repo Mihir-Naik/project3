f04093c370bb91a02e549894062e8eae3ea9850c
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-10-17 14:29:39 -0700

    Add my properties page

--- routes/properties.js
+++ routes/properties.js
@@ -6,6 +6,7 @@ const
   propertiesCtrl = require('../controllers/properties.js')
 
 propertyRouter.get('/new', propertiesCtrl.new)
+propertyRouter.get('/my_properties', propertiesCtrl.currentUserProperties)
 
 propertyRouter.route('/:id')
   .get(propertiesCtrl.show)
@@ -19,8 +20,5 @@ propertyRouter.get('/:id/edit', propertiesCtrl.edit)
 propertyRouter.route('/')
   .post(propertiesCtrl.create)
   .get(propertiesCtrl.index)
-
-propertyRouter.get('/my_properties', propertiesCtrl.currentUserProperties)
-
 
 module.exports = propertyRouter
