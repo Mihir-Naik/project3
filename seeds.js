6714fd93998db5c98e614ea74e0a21b7ae2cedc8
Mihir Naik <mdnaik17@gmail.com>
2017-10-16 20:39:52 -0700

    seeds file for 3 users with 1 property each

--- seeds.js
+++ seeds.js
@@ -9,6 +9,8 @@ mongoose.connect(mongoConnectionString, (err) => {
   console.log(err || `Connected to MongoDB @ ${mongoConnectionString}`)
 })
 
+// *** user seeds with properties *** //
+
 var user1params = {
   firstName: "Jane",
   lastName: "Doe",
@@ -37,45 +39,111 @@ var user3params = {
 }
 
 User.find({firstName: "Jane", lastName: "Doe"}, (req, users) => {
-  if(users.length > 1) {
+  if(users.length) {
     return console.log('User1 already exists')
   }
   else {
     console.log('im here 1')
     var user1 = new User(user1params)
     user1.password = user1.generateHash('janedoe')
-    return user1.save()
+    return user1.save(() => {
+      var newProperty1 = new Property()
+      newProperty1.aptNumber = "4565";
+      newProperty1.street = "5th Ave";
+      newProperty1.city = "Los Angeles";
+      newProperty1.zipCode = 90006;
+      newProperty1.state = "CA";
+      newProperty1.rentalPrice = 2500;
+      newProperty1.numBedrooms = 1;
+      newProperty1.numBathrooms = 1;
+      newProperty1.sizeSquareFeet = 700;
+      newProperty1.vacant = true;
+      newProperty1.description = "Here is the description of property that describes the property...";
+      newProperty1.owner = user1._id;
+      newProperty1.save(function(err) {
+        if (err) console.log(err);
+        else console.log('saved');
+        user1.ownedProperties.push(newProperty1)
+        user1.save();
+      })
+    })
   }  
 })
 
 User.find({firstName: "Scarlet", lastName: "Johnson"}, (req, users) => {
-  if(users.length > 1) {
+  if(users.length) {
     return console.log('User2 already exists')
   }
   else {
     console.log('im here 2')
     var user2 = new User(user2params)
     user2.password = user2.generateHash('johnson')
-    return user2.save()
+    return user2.save(() => {
+      var newProperty2 = new Property()
+      newProperty2.aptNumber = "123";
+      newProperty2.street = "Main Street East";
+      newProperty2.city = "Santa Monica";
+      newProperty2.zipCode = 90401;
+      newProperty2.state = "CA";
+      newProperty2.rentalPrice = 2800;
+      newProperty2.numBedrooms = 2;
+      newProperty2.numBathrooms = 2;
+      newProperty2.sizeSquareFeet = 800;
+      newProperty2.vacant = true;
+      newProperty2.description = "Here is the description of property that describes the property...";
+      newProperty2.owner = user2._id;
+      newProperty2.save(function(err) {
+        if (err) console.log(err);
+        else console.log('saved');
+        user2.ownedProperties.push(newProperty2)
+        user2.save();
+      })
+    })
   }  
 })
 
 User.find({firstName: "Bob", lastName: "Smith"}, (req, users) => {
-  if(users.length > 1) {
+  if(users.length) {
     return console.log('User already exists 3')
   }
   else {
     console.log('im here 3')
     var user3 = new User(user3params)
     user3.password = user3.generateHash('bobsmith')
-    return user3.save()
+    return user3.save(() => {
+      var newProperty3 = new Property()
+      newProperty3.aptNumber = "333";
+      newProperty3.street = "7th Street East";
+      newProperty3.city = "Los Angeles";
+      newProperty3.zipCode = 90016;
+      newProperty3.state = "CA";
+      newProperty3.rentalPrice = 2300;
+      newProperty3.numBedrooms = 2;
+      newProperty3.numBathrooms = 1;
+      newProperty3.sizeSquareFeet = 900;
+      newProperty3.vacant = true;
+      newProperty3.description = "Here is the description of property that describes the property...";
+      newProperty3.owner = user3._id;
+      newProperty3.save(function(err) {
+        if (err) console.log(err);
+        else console.log('saved');
+        user3.ownedProperties.push(newProperty3)
+        user3.save();
+      })
+    })
   }   
 })
 
-console.log('end of seeds')
+/*
+function dropCollection(collection) {
+  if (mongoose.connection.collections[collection]) {
+    mongoose.connection.collections[collection].drop((err)=>{
+      if (err) console.log(err);
+      else console.log(collection + ' dropped')
+    })
+  }
+}
 
-// User.find({email}, (err, user) => {
-//   var newProperty = new Property
-//   newProperty.owner = user._id
-//   newProperty.save()
-// })
+dropCollection('users');
+dropCollection('properties');
+*/
